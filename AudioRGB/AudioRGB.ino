b0b5458a54e1bc7144a1296468fd541b64defd7b
FabianBartl <fabian@informatic-freak.de>
2022-07-27 20:00:53 +0200

    delay, prescale, loop, setup

--- AudioRGB/AudioRGB.ino
+++ AudioRGB/AudioRGB.ino
@@ -1,15 +1,19 @@
 #include <Arduino.h>
+#include <util/delay.h>
 
 #define LED_R 11
 #define LED_G 10
 #define LED_B 9
 #define MIC_D 12
 #define MIC_A A0
+#define DELAY 1
+#define PRESCALE 64
 
-unsigned long int time = 0;
-volatile int mic_a = 0;
+volatile unsigned long int time=0, timer=0;
+volatile int mic_a=0;
+int r=0, g=0, b=0;
 
-void init()
+void setup()
 {
   pinMode(LED_R, OUTPUT);
   pinMode(LED_G, OUTPUT);
@@ -18,38 +22,33 @@ void init()
   pinMode(MIC_A, INPUT);
 }
 
-void writeRGB(int r, int g, int b)
+void writeRGB(float r, float g, float b)
 {
-  analogWrite(LED_R, r);
-  analogWrite(LED_G, g);
-  analogWrite(LED_B, b);
+  analogWrite(LED_R, fit(r));
+  analogWrite(LED_G, fit(g));
+  analogWrite(LED_B, fit(b));
 }
 
-int fit(int v) { return v<0 ? 0 : (v>255 ? 255 : v); }
-int fit(float v) { return fit((int)v); }
+int fit(float v) { return v<0 ? 0 : (v>255 ? 255 : (int)v); }
 
-int genR(int v) { return fit(127.5 * sin(0.001 * v * time) + 127.5); }
-int genG(int v) { return genR(v); }
-int genB(int v) { return genR(v); }
+int genR(int v) { return 127.5 * sin(0.01 * (float)v * (float)time) + 127.5; }
+int genG(int v) { return time; }
+int genB(int v) { return 0; }
 
-int main()
+void loop()
 {
-  init();
   Serial.begin(9600);
-
   while (1)
   {
     mic_a = analogRead(MIC_A);
+    r = genR(mic_a);
+    g = genG(mic_a);
+    b = genB(mic_a);
+    writeRGB(r, g, b);
     Serial.println(time);
-    
-    writeRGB(
-      genR(mic_a),
-      genG(mic_a),
-      genB()
-    );
-    
-    time++;
-  }
 
-  return 0;
+    timer++;
+    time = timer / PRESCALE;
+    _delay_ms(DELAY);
+    }
 }

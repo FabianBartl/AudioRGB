1f75589c6cd8a4dfcbdb03bdfb674725eac66517
FabianBartl <fabian@informatic-freak.de>
2022-08-08 00:27:24 +0200

    min / max for types, min(), max(), minArray(), maxArray(), generatorLimit()

--- AudioRGB_STM/Core/Src/functions.c
+++ AudioRGB_STM/Core/Src/functions.c
@@ -9,8 +9,6 @@
 #include "tim.h"
 #include "adc.h"
 
-#define min(a,b) (a<b?a:b)
-
 // ---------------
 // circular buffer
 // ---------------
@@ -28,7 +26,7 @@ void bufferAppend(int val, int *arr, int *ind)
 int bufferFilter(int *arr, int *ind)
 {
 	int sum = 0;
-	for(int i=0, p=0; i < FILTER_SIZE_AUX; i++)
+	for(size_t i=0, p=0; i < FILTER_SIZE_AUX; i++)
 	{
 		p = (*ind-i < 0) ? BUFFER_SIZE_AUX-1 : *ind-i;
 		sum += arr[p];
@@ -52,6 +50,13 @@ int transform(int val, int inMin, int inMax, int outMin, int outMax)
 
 int amplifyFactor(int val, int fac) { return val * fac; }
 int amplify(int val) { return amplifyFactor(val, VOLUME_BOOST); }
+
+// --------
+// min, max
+// --------
+
+int min(int a, int b) { return (a < b) ? a : b; }
+int max(int a, int b) { return (a > b) ? a : b; }
 
 // ---------------
 // noise generator
@@ -84,28 +89,43 @@ void writeRGB(int r, int g, int b)
 void writeRGBArray(int *rgb) { writeRGB(rgb[0], rgb[1], rgb[2]); }
 
 // color generator
-int generator(int val)
+int generatorLimit(int val, int lowerLim, int upperLim)
 {
 	return transform(
 		amplify(val),
-		VOLUME_MIN,
-		VOLUME_MAX,
+		lowerLim,
+		upperLim,
 		COLOR_MIN,
 		COLOR_MAX
 	);
 }
+int generator(int val) { return generatorLimit(val, VOLUME_MIN, VOLUME_MAX); }
 
 // ------
 // arrays
 // ------
 
-void fillArray(int val, int *arr, const size_t arrLen) { for(int i=0; i < arrLen; i++) arr[i] = val; }
+void fillArray(int val, int *arr, const size_t arrLen) { for(size_t i=0; i < arrLen; i++) arr[i] = val; }
 void emptyArray(int *arr, const size_t arrLen) { fillArray(0, arr, arrLen); }
 
 int arraySum(int *arr, const size_t arrLen)
 {
 	int sum = 0;
-	for(int i=0; i < arrLen; i++) sum += arr[i];
+	for(size_t i=0; i < arrLen; i++) sum += arr[i];
 	return sum;
 }
 int arrayAvr(int *arr, const size_t arrLen) { return arraySum(arr, arrLen) / arrLen; }
+
+int arrayMin(int *arr, const size_t arrLen)
+{
+	int minVal = MAX_INT32;
+	for (size_t i=0; i < arrLen; i++) minVal = min(minVal, arr[i]);
+	return minVal;
+}
+int arrayMax(int *arr, const size_t arrLen)
+{
+	int maxVal = MIN_INT32;
+	for (size_t i=0; i < arrLen; i++) maxVal = max(maxVal, arr[i]);
+	return maxVal;
+}
+
